80e2b8f16892e24d0e26cf1100625e6959b7382d
Julien Duseyau <julien.duseyau@gmail.com>
2017-08-13 18:32:19 +0200

    Preparing tests

---
+++ tests/DatabaseTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Maduser\Minimal\Database\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DatabaseTest extends TestCase
+{
+
+}
--- tests/ExampleTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Maduser\Minimal\Database\Tests;
-
-class ExampleTest extends \PHPUnit_Framework_TestCase
-{
-
-}
